8428b90c2c5f29b153cf67e11ba2d4d9f6201d58
Arshathkky <Arshathhaseen@gmail.com>
2023-12-19 22:54:22 +0530

    after  server.js file

---
+++ Backend/Server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// MongoDB connection
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+// ... Other routes and configurations ...
+
+app.listen(port, () => {
+    connectDB();
+    console.log(`App running on ${port}`);
+});
