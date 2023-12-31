3b9320b8a0128e937ad5a43e7a784fb61afc9da2
Arshathkky <Arshathhaseen@gmail.com>
2023-12-31 19:03:24 +0530

    after Layout folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { AdminLayout } from "./Layout/AdminLayout";
+import { UserLayout } from "./Layout/UserLayout";
 
+const role = 'user'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  {
+    if (role == 'user')
+    return(
+      <UserLayout/>
+    )
+    else if (role == 'admin')
+    return(
+      <AdminLayout/>
+    )
+  }
 }
 
 export default App;
