b067e67ab796ef6d1a2e393089d46501324151d4
Arshathkky <Arshathhaseen@gmail.com>
2024-01-13 17:28:35 +0530

    updated templete

---
+++ frontend/src/components/homeComponent.js/Sliderbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sliderbar = () => {
+  return (
+    <div>
+      Sliderbar
+    </div>
+  )
+}
+
+export default Sliderbar
---
+++ frontend/src/pages/adminPages/AddStaff.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddStaff = () => {
+  return (
+    <div>
+      AddStaff
+    </div>
+  )
+}
+
+export default AddStaff
