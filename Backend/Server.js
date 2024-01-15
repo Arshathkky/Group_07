ad59ecdb02a0661e51d90a034adf9a3783592255
Arshathkky <Arshathhaseen@gmail.com>
2024-01-15 23:32:41 +0530

    Setup mongodb

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,35 +1,17 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-
-dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 5000;
+
+mongoose.connect('mongodb+srv://arshathhaseen:1234@cluster0.4ahayis.mongodb.net/SportManagementSystem');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
-// MongoDB connection
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error(error.message);
-    }
-}
 
-// ... Other routes and configurations ...
 
 app.listen(port, () => {
-    connectDB();
     console.log(`App running on ${port}`);
 });
