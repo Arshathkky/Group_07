460b55c0d79df78b4b130a569a600aa1cffd120c
Kavindu <kavinduhirushan2477025@gmail.com>
2024-01-17 20:22:03 +0530

    after News area

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,27 +3,17 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const port = 5000;
-<<<<<<< HEAD
 const eventRouter = require('./Routes/EventRoutes')
-=======
-
+const NewsRoute = require('./Routes/NewsRoutes')
 mongoose.connect('mongodb+srv://arshathhaseen:1234@cluster0.4ahayis.mongodb.net/SportManagementSystem');
->>>>>>> 32e0bde5bd9c2ea3dd807fbe58c1af4a389ed9cf
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-// MongoDB connection
-mongoose.connect('mongodb+srv://arshathhaseen:1234@cluster0.4ahayis.mongodb.net/SportManagementSystem');
 app.use('/api/event',eventRouter)
-
-
-=======
->>>>>>> 32e0bde5bd9c2ea3dd807fbe58c1af4a389ed9cf
-
+app.use('/api/news',NewsRoute)
 
 app.listen(port, () => {
-    console.log(`App running on ${port}`);
+    console.log('App running on ${port}');
 });
--- frontend/src/pages/userPages/Home.js
+++ frontend/src/pages/userPages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import Sliderbar from  '../../components/homeComponent.js/Sliderbar'
-
+import News from '../../components/homeComponent.js/News'
+import Event from '../../components/homeComponent.js/Events'
 const Home = () => {
   return (
     <div>
         <Sliderbar/>
+        <Event/>
+        <News/>
     </div>
   )
 }
